9b8550c8a023dbb3973919d30bbeb8ce0c70a336
salomesch <salome.schwegler@stud.unilu.ch>
2026-03-07 15:35:23 +0100

    Conntection to API, figuring out data format and extracting 100 articles about endometriosis. Extract certain variables of dataset. Build basic structure of data set.

--- scripts/01-mining.R
+++ scripts/01-mining.R
@@ -1,1 +1,44 @@
+
+# load libraries ----------------------------------------------------------
 library(tidyverse)
+library(httr)
+library(rvest)
+library(jsonlite)
+
+
+response <-  httr::GET(
+  url = "https://api.gdeltproject.org/api/v2/doc/doc?query=%22endometriosis%22&mode=artlist&maxrecords=100&timespan=1week",
+  verbose()
+)
+
+response <- httr::GET(
+  "https://api.gdeltproject.org/api/v2/doc/doc",
+  query = list(
+    query = "endometriosis",
+    mode = "artlist",
+    maxrecords = 100,
+    timespan = "1week",
+    format = "json"
+  )
+)
+data <- content(response, as = "parsed")
+
+# Create empty vectors where to store the codes and names:
+out_url <- vector(mode = "character", length = length(data$articles))
+out_title <- vector(mode = "character", length = length(data$articles))
+out_sourcecountry <- vector(mode = "character", length = length(data$sourcecountry))
+
+# Now loop to extract all codes/names from the list:
+for (i in 1:length(data$articles)){
+  out_url[i] <- data$articles[[i]]$url
+  out_title[i] <- data$articles[[i]]$title
+  out_sourcecountry[i] <- data$articles[[i]]$sourcecountry
+}
+
+# 5. Convert into a tidy tibble --------------------
+variables_dataset = tibble(
+  url = out_url, 
+  title = out_title,
+  country = out_sourcecountry
+)
+

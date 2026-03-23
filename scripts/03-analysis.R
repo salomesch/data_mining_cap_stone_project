4b75bea7342de1380a3ec8249242a97ee55decd5
salomesch <salome.schwegler@stud.unilu.ch>
2026-03-23 16:13:22 +0100

    sentiment plot by month for each year. however there is no clear pattern observable.

--- scripts/03-analysis.R
+++ scripts/03-analysis.R
@@ -12,7 +12,7 @@ average_sentiments_yearly <- timeline_df_preprocessed |>
   summarise(sentiment = mean(value, na.rm=T))
 
 # create a bar plot of the average sentiment score per year
-sentiment_plot <- ggplot(average_sentiments_yearly, aes(x = factor(year), y = sentiment)) +
+sentiment_plot_yearly <- ggplot(average_sentiments_yearly, aes(x = factor(year), y = sentiment)) +
   geom_bar(stat = "identity", colour = "black", fill = "#2171B5", alpha = 0.7) +
   geom_text(
     aes(label = round(sentiment, 2)), 
@@ -31,7 +31,33 @@ sentiment_plot <- ggplot(average_sentiments_yearly, aes(x = factor(year), y = se
 
 
 # display the plot
-print(sentiment_plot)
+print(sentiment_plot_yearly)
 
+
+# create a bar plot of average sentiment score per month for all years
+average_sentiments_month_yearly <- timeline_df_preprocessed |> 
+  group_by(year, month) |> 
+  summarise(sentiment = mean(value, na.rm=T), .groups = "drop")
+
+# the score has become slightly less negative over the years
+
+
+# plot
+sentiment_plot_monthly <- ggplot(average_sentiments_month_yearly, aes(x = factor(month), y = sentiment)) +
+  geom_bar(stat = "identity", fill = "#6BAED6", color = "black", alpha = 0.7) +
+  facet_wrap(~ year, ncol = 2) +
+  ggtitle("Average Sentiment Score per Month over the Years") +
+  xlab("Month") +
+  ylab("Average Sentiment Score") +
+  theme_minimal() +
+  theme(plot.title = element_text(hjust = 0.5, face = "bold"), 
+        axis.text.x = element_text(size = 12),
+        axis.title.x = element_text(size = 14),
+        axis.title.y = element_text(size = 14)) +
+  scale_x_discrete(labels = month.abb)
+
+sentiment_plot_monthly
+
+# no clear pattern
 
 
